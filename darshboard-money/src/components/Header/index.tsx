9cd2acba174bf2f92bac8fe2902e4a30bfd4174c
LeoMedeirosss <lmf@cesar.school>
2025-08-15 17:02:08 -0300

    feat(modal style)

--- darshboard-money/src/components/Header/index.tsx
+++ darshboard-money/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderConteiner, HeaderContent, Icon, NewTransactionButton } from "./styles";
 import { CircleAlertIcon } from "lucide-react"
 import * as Dialog from "@radix-ui/react-dialog"
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return(
@@ -16,14 +17,7 @@ export function Header() {
                         <NewTransactionButton>Nova transação</NewTransactionButton>
                     </Dialog.Trigger>
 
-                    <Dialog.Portal>
-                        <Dialog.Overlay />
-
-                        <Dialog.Content>
-                            <Dialog.Title>Nova transação</Dialog.Title>
-                            <Dialog.Close/>
-                        </Dialog.Content>
-                    </Dialog.Portal>
+                    <NewTransactionModal />
                 </Dialog.Root>
                 
             </HeaderContent>
